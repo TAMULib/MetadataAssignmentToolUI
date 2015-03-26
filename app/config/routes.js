3433d4f144919ee71b8cbe05bc5c91939c9c7742
James Creel <jcreel@tamu.edu>
2015-03-26 08:50:33 -0500

    Admin view routeprovided in routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,6 +4,9 @@ myLibrary.config(['$routeProvider', '$locationProvider', function($routeProvider
 		when('/myview', {
 			templateUrl: 'view/myview.html'
 		}).
+		when('/admin', {
+			templateUrl: 'view/admin.html'
+		}).
 		otherwise({redirectTo: '/',
 			templateUrl: 'view/index.html'
 		});
