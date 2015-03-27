4156a4e0447decd7a4327814ff47fab50259a4cc
William Welling <wwelling@library.tamu.edu>
2015-03-27 13:08:44 -0500

    Added document route.

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,6 +1,9 @@
 myLibrary.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$locationProvider.html5Mode(true);
 	$routeProvider.
+		when('/documents', {
+			templateUrl: 'view/documents.html'
+		}).
 		when('/myview', {
 			templateUrl: 'view/myview.html'
 		}).
