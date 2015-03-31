a2cab1cdeaf2cd0ea20d6fcf8dde5ec7bae5948a
William Welling <wwelling@library.tamu.edu>
2015-03-31 12:54:31 -0500

    Ability to claim/assign documents based on self role.

--- app/controllers/documentController.js
+++ app/controllers/documentController.js
@@ -1,6 +1,51 @@
-metadataTool.controller('DocumentController', function ($scope, Document) {
+metadataTool.controller('DocumentController', function ($scope, DocumentRepo, User, UserRepo) {
 
-	$scope.documents = Document.get();
+	var userRepo;
+	
+	var annotators = [];
+	
+	$scope.documents = DocumentRepo.get();
+	
+	$scope.isAdmin = function() {
+		if(sessionStorage.role == "ROLE_ADMIN") {
+			return true;
+		}
+		return false;
+	};
+	
+	$scope.isManager = function() {
+		if(sessionStorage.role == "ROLE_MANAGER") {
+			return true;
+		}
+		return false;
+	};
+	
+	$scope.availableAnnotators = function() {		
+		if(!userRepo) {
+			userRepo = UserRepo.get();
+			for(var key in userRepo.list) {
+				var user = userRepo.list[key];
+				if(user.role == 'ROLE_ANNOTATOR') {
+					annotators.push(user);
+				}
+			}
+		}		
+		return annotators;
+	};
+	
+	$scope.updateAnnotator = function(filename, annotator) {
+		if(!annotator) {
+			annotator = User.get();
+		}
+		else {
+			annotator = JSON.parse(annotator);
+		}
+		DocumentRepo.updateAnnotator(filename, annotator.uin);
+	};
+	
+	$scope.test = function() {
+		return "claim";
+	}
 	
 });
 
---
+++ tests/unit/models/documentRepoModelTest.js
@@ -0,0 +1,44 @@
+
+describe('model: DocumentRepo', function() {
+	
+	var DocumentRepo, WsApi;
+	
+	beforeEach(module('metadataTool'));
+	
+	beforeEach(module('mock.wsApi'));
+	
+	beforeEach(inject(function(_DocumentRepo_, _WsApi_) {
+		DocumentRepo = _DocumentRepo_;
+        WsApi = _WsApi_; 
+    }));
+
+	describe('model is defined', function() {
+		it('should be defined', function() {
+			expect(DocumentRepo).toBeDefined();
+		});
+	});
+	
+	describe('get method should return a Document', function() {
+		it('the Document was returned', function() {
+			expect(DocumentRepo.get().content).toEqual(mockDocumentRepo1);
+		});
+	});
+
+	describe('set method should set a Document', function() {
+		it('the Document was set', function() {
+			var documents = DocumentRepo.get();
+			DocumentRepo.set({"unwrap":function(){}, "content":mockDocumentRepo2});
+			expect(documents.content).toEqual(mockDocumentRepo2);
+		});
+	});
+	
+	describe('update method should udpate a document in the DocumentRepo', function() {
+		it('the document was updated in the DocumentRepo', function() {
+			var docRepo = DocumentRepo.get();			
+			DocumentRepo.updateAnnotator("disseration001.txt", "222222222");
+			docRepo.content['HashMap'].annotator = "222222222";
+			expect(docRepo.content).toEqual(mockDocumentRepo1);
+		});
+	});
+
+});
