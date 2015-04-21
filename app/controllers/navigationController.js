194c8a63ac70355ca29083acf213e369a83d6d91
jeremythuff <jeremythuff@gmail.com>
2015-04-21 01:27:41 -0500

    added navigation detection and brought in more branded colors

---
+++ app/controllers/navigationController.js
@@ -0,0 +1,11 @@
+metadataTool.controller('NavigationController', function ($controller, $scope, $location) {
+	
+    angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+
+    $scope.$on('$routeChangeStart', function(next, current) {
+	    $scope.view = $location.$$path;  
+	});
+	
+});
+
+
