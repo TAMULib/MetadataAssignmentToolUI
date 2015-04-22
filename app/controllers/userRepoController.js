c4a28ba9fab5b8f0c172649fbb391f3c65a3aa79
William Welling <wwelling@library.tamu.edu>
2015-04-21 23:05:04 -0500

    Moved user repo specific methods and scope from admin controller into a new user repo controller.

---
+++ app/controllers/userRepoController.js
@@ -0,0 +1,46 @@
+metadataTool.controller('UserRepoController', function ($controller, $location, $scope, $route, User, UserRepo) {
+	
+    angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+       
+    $scope.userRepo = UserRepo.get();
+     
+    User.ready().then(function(){
+    	
+		$scope.updateRole = function(uin, role) {
+			UserRepo.updateRole(uin, role);
+			
+			console.log($scope.user.uin);
+			console.log(uin);
+			
+			if($scope.user.uin == uin) {
+				if(role == 'ROLE_ANNOTATOR') {
+					$location.path('/assignments');
+				}
+				else if(role == 'ROLE_USER') {
+					$location.path('/myview');
+				}
+				else {}
+			}
+			
+		};
+		
+		$scope.allowableRoles = function(userRole) {
+			if(sessionStorage.role == 'ROLE_ADMIN') {
+				return ['ROLE_ADMIN','ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
+			}
+			else if(sessionStorage.role == 'ROLE_MANAGER') {
+				if(userRole == 'ROLE_ADMIN') {
+					return ['ROLE_ADMIN'];
+				}
+				return ['ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
+			}
+			else {
+				return [userRole];
+			}
+		};
+		
+    });
+	
+});
+
+
