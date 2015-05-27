0a50c9d95c6654a7c4e9ff4d94f93693234fb68b
William Welling <wwelling@library.tamu.edu>
2015-05-27 12:56:50 -0500

    Updated annotate controller.

--- app/controllers/annotateController.js
+++ app/controllers/annotateController.js
@@ -16,87 +16,95 @@ metadataTool.controller('AnnotateController', function($controller, $scope, $loc
 
 	$scope.cv = ControlledVocabulary.get();
 
-	$scope.getControlledVocabulary = function(label) {
-		if(typeof $scope.cv[label] === 'undefined') {
-			return [];
-		}
-		return $scope.cv[label];
-	};
+	ControlledVocabulary.ready().then(function() {
 
-	DocumentRepo.ready().then(function() {
+		DocumentRepo.ready().then(function() {
 		
-		angular.extend($scope.document, {'metadata':Metadata.get($scope.document)});
-		
-		Metadata.ready().then(function() {
-			for(var key in $scope.document.metadataLabels) {
-				var metadataLabel = $scope.document.metadataLabels[key];
-				if(!$scope.document.metadata[metadataLabel.label]) {
-					if (metadataLabel.defaultValue) {
-						$scope.document.metadata[metadataLabel.label] = [metadataLabel.defaultValue];
-					} else {
-						$scope.document.metadata[metadataLabel.label] = [''];
+			angular.extend($scope.document, {'metadata':Metadata.get($scope.document)});
+			
+			Metadata.ready().then(function() {
+
+				for(var key in $scope.document.metadataLabels) {
+					var metadataLabel = $scope.document.metadataLabels[key];
+					if(!$scope.document.metadata[metadataLabel.label]) {
+						if (metadataLabel.defaultValue) {
+							$scope.document.metadata[metadataLabel.label] = [metadataLabel.defaultValue];
+						} else {
+							$scope.document.metadata[metadataLabel.label] = [''];
+						}
 					}
-				}
-			}		
-			$scope.removeMetadataField = function(label) {
-				$scope.document.metadata[label].splice(Object.keys($scope.document.metadata[label]).length-1, 1);
-			};
-			
-			$scope.addMetadataField = function(label,defaultValue) {
-				$scope.document.metadata[label][Object.keys($scope.document.metadata[label]).length] = (defaultValue) ? defaultValue:'';
-			};
-			
-			$scope.getMetadataFieldCount = function(label) {
-				return Object.keys($scope.document.metadata[label]).length;
-			};
-			
-			$scope.updateMetadata = function(document) {
-				Metadata.clear(document.name).then(function(data) {								
-					Metadata.add(document.name, document.metadata);
-				});
-			};
-			
-			$scope.submit = function(document) {				
-				Metadata.clear(document.name).then(function(data) {					
-					Metadata.add(document.name, document.metadata);
-					DocumentRepo.update(document.name, $scope.user, 'Annotated', '');
-					$location.path('/assignments');					
-				});
-			};
-			
-			$scope.accept = function(document) {
-				Metadata.clear(document.name).then(function(data) {					
-					Metadata.add(document.name, document.metadata);
-					DocumentRepo.update(document.name, $scope.document.annotator, 'Published', '');
-					$location.path('/documents');					
-				});
-			};
-
-			$scope.managerAnnotating = function() {
-				return ($routeParams.action == 'annotate');
-			};
-			
-			$scope.managerReviewing = function() {
-				return ($routeParams.action == 'review');
-			};
-			
-			$scope.submitRejection = function(rejectionNotes) {
-				if(rejectionNotes) {
-					DocumentRepo.update($scope.document.name, $scope.document.annotator, 'Rejected', rejectionNotes).then(function() {
-						$timeout(function() {
-							$location.path('/documents');
-						}, 500);
+				}		
+				$scope.removeMetadataField = function(label) {
+					$scope.document.metadata[label].splice(Object.keys($scope.document.metadata[label]).length-1, 1);
+				};
+				
+				$scope.addMetadataField = function(label,defaultValue) {
+					$scope.document.metadata[label][Object.keys($scope.document.metadata[label]).length] = (defaultValue) ? defaultValue:'';
+				};
+				
+				$scope.getMetadataFieldCount = function(label) {
+					return Object.keys($scope.document.metadata[label]).length;
+				};
+				
+				$scope.updateMetadata = function(document) {
+					Metadata.clear(document.name).then(function(data) {								
+						Metadata.add(document.name, document.metadata);
 					});
-				}
-			};
+				};
+				
+				$scope.submit = function(document) {				
+					Metadata.clear(document.name).then(function(data) {					
+						Metadata.add(document.name, document.metadata);
+						DocumentRepo.update(document.name, $scope.user, 'Annotated', '');
+						$location.path('/assignments');					
+					});
+				};
+				
+				$scope.accept = function(document) {
+					Metadata.clear(document.name).then(function(data) {					
+						Metadata.add(document.name, document.metadata);
+						DocumentRepo.update(document.name, $scope.document.annotator, 'Published', '');
+						$location.path('/documents');					
+					});
+				};
+
+				$scope.managerAnnotating = function() {
+					return ($routeParams.action == 'annotate');
+				};
+				
+				$scope.managerReviewing = function() {
+					return ($routeParams.action == 'review');
+				};
+				
+				$scope.submitRejection = function(rejectionNotes) {
+					if(rejectionNotes) {
+						DocumentRepo.update($scope.document.name, $scope.document.annotator, 'Rejected', rejectionNotes).then(function() {
+							$timeout(function() {
+								$location.path('/documents');
+							}, 500);
+						});
+					}
+				};
+				
+				$scope.requiresCuration = function(name) {
+					DocumentRepo.update(name, $scope.user, 'Requires Curation');
+					$location.path('/assignments');
+				};
+
+				$scope.getControlledVocabulary = function(label) {
+					console.log(label);
+					if(typeof $scope.cv[label] === 'undefined') {
+						return [];
+					}
+					console.log($scope.cv[label]);
+					return $scope.cv[label];
+				};
+				
+			});	
 			
-			$scope.requiresCuration = function(name) {
-				DocumentRepo.update(name, $scope.user, 'Requires Curation');
-				$location.path('/assignments');
-			};
-			
-		});	
-		
+		});
+
 	});
+	
 	
 });
