794850a8965f36bef5c3fa0cf749d7da1ae5aad3
jsavell <jsavell@library.tamu.edu>
2017-01-20 14:40:13 -0600

    improve batch publishing ux

--- app/controllers/batchPublishController.js
+++ app/controllers/batchPublishController.js
@@ -1,18 +1,26 @@
-metadataTool.controller('BatchPublishController', function ($controller, $scope, ProjectRepo) {
+metadataTool.controller('BatchPublishController', function ($controller, $scope, ProjectRepo, AlertService) {
     angular.extend(this, $controller('AbstractController', {$scope: $scope}));
 
     $scope.projects = ProjectRepo.getAll();
 
-	$scope.batchProject = {};
-
-	$scope.batchRepository = {};
+	$scope.isPublishing = false;
 
     ProjectRepo.ready().then(function() {
         $scope.batchProject = $scope.projects[0];
-		$scope.batchRepository = $scope.batchProject.repositories[0];
+		if ($scope.batchProject.repositories[0]) {
+			$scope.batchRepository = $scope.batchProject.repositories[0];
+		} else {
+			$scope.batchRepository = {};
+		}
 
-		$scope.publishDocuments = function() {
-			ProjectRepo.batchPublishDocuments($scope.batchProject.id,$scope.batchRepository.id);
+		$scope.publishDocuments = function(project,repository) {
+			$scope.isPublishing = true;
+			ProjectRepo.batchPublishDocuments(project.id,repository.id).then(function(rawResponse) {
+				var response = angular.fromJson(rawResponse.body);
+                AlertService.add(response.meta, "app/projects");
+				$scope.isPublishing = false;
+				$scope.closeModal();
+			});
 		};
     });
 
