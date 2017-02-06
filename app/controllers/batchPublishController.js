099d3831ff30f2cb03914eb42df15d5fdb398f1c
William Welling <wwelling@library.tamu.edu>
2017-02-06 11:39:29 -0600

    Check if any projects available for batch publish.

--- app/controllers/batchPublishController.js
+++ app/controllers/batchPublishController.js
@@ -1,27 +1,32 @@
-metadataTool.controller('BatchPublishController', function ($controller, $scope, ProjectRepo, AlertService) {
-    angular.extend(this, $controller('AbstractController', {$scope: $scope}));
+metadataTool.controller('BatchPublishController', function($controller, $scope, ProjectRepo, AlertService) {
+    angular.extend(this, $controller('AbstractController', {
+        $scope: $scope
+    }));
 
     $scope.projects = ProjectRepo.getAll();
 
-	$scope.isPublishing = false;
+    $scope.isPublishing = false;
 
     ProjectRepo.ready().then(function() {
-        $scope.batchProject = $scope.projects[0];
-		if ($scope.batchProject.repositories[0]) {
-			$scope.batchRepository = $scope.batchProject.repositories[0];
-		} else {
-			$scope.batchRepository = {};
-		}
+        if ($scope.projects.length > 0) {
+            $scope.batchProject = $scope.projects[0];
 
-		$scope.publishDocuments = function(project,repository) {
-			$scope.isPublishing = true;
-			ProjectRepo.batchPublishDocuments(project.id,repository.id).then(function(rawResponse) {
-				var response = angular.fromJson(rawResponse.body);
-                AlertService.add(response.meta, "app/projects");
-				$scope.isPublishing = false;
-				$scope.closeModal();
-			});
-		};
+            if ($scope.batchProject.repositories[0]) {
+                $scope.batchRepository = $scope.batchProject.repositories[0];
+            } else {
+                $scope.batchRepository = {};
+            }
+
+            $scope.publishDocuments = function(project, repository) {
+                $scope.isPublishing = true;
+                ProjectRepo.batchPublishDocuments(project.id, repository.id).then(function(rawResponse) {
+                    var response = angular.fromJson(rawResponse.body);
+                    AlertService.add(response.meta, "app/projects");
+                    $scope.isPublishing = false;
+                    $scope.closeModal();
+                });
+            };
+        }
     });
 
 });
