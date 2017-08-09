f3bf0b1a000c8b2e414d1d83b42a2e9bd38ec620
jeremythuff <jeremythuff@gmail.com>
2017-08-09 16:20:14 -0500

    Completed node webserver

--- app/app.js
+++ app/app.js
@@ -1,48 +1,104 @@
-var metadataTool = angular.module('metadataTool',
-[
+// var metadataTool = angular.module('metadataTool',
+// [
+//     'ngRoute',
+//     'ngTable',
+//     'ngSanitize',
+//     'ngCsv',
+//     'ngFileUpload',
+//     'metadataTool.version'
+// ]);
+
+// metadataTool.model = core.model;
+// metadataTool.repo = core.repo;
+
+// // This method's callback is passed to stomp and executed on both successfull connection, as well as disconnect.
+// setUpApp(function(connected) {
+
+//     // Indicates at app start if the app has successfully conenected to the service
+//     appConfig.connected = connected;
+
+//     metadataTool.constant('appConfig', appConfig);
+//     metadataTool.constant('api', apiMapping);
+
+//     angular.element(document).ready(function() {
+
+//         var doc = angular.element(document);
+//         var injector = doc.injector();
+//         try {
+
+//             // If the app is already bootstrapped then an error will be thrown
+//             angular.bootstrap(document, ['core', 'metadataTool', 'ui.bootstrap']);
+            
+//             if(!window.stompClient.connected) {
+//                 AlertService = injector.get('AlertService');
+//                 AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
+//             }
+//         } catch (e) {
+
+//             /*
+//              * If websockets dissconnect the app will attempt to re-bootstrap. Since the app is already running we will
+//              * end up in this block, and can generate an error indicating the disconnect.
+//              */
+//             if(typeof injector != 'undefined') {
+//                 AlertService = injector.get('AlertService');
+//                 AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
+//             }
+//         }
+//     });
+
+// });
+
+var metadataTool = angular.module('metadataTool', [
     'ngRoute',
     'ngTable',
     'ngSanitize',
     'ngCsv',
-    'ngFileUpload',
-    'metadataTool.version'
+    'ngFileUpload'
 ]);
 
 metadataTool.model = core.model;
 metadataTool.repo = core.repo;
 
-// This method's callback is passed to stomp and executed on both successfull connection, as well as disconnect.
-setUpApp(function(connected) {
+var unreachableAlert = function (injector) {
+    if (injector !== undefined) {
+        AlertService = injector.get('AlertService');
+        AlertService.add({
+            type: "ERROR",
+            message: "Web service cannot be reached."
+        }, "/app/errors");
+    } else {
+        //foreward to error page
+        console.log('forward to error page');
+    }
+};
 
-    // Indicates at app start if the app has successfully conenected to the service
+//This method's callback is passed to stomp and executed on both successfull connection, as well as disconnect.
+setUpApp(function (connected) {
+
+    //Indicates at app start if the app has successfully conenected to the service
     appConfig.connected = connected;
 
     metadataTool.constant('appConfig', appConfig);
     metadataTool.constant('api', apiMapping);
 
-    angular.element(document).ready(function() {
-
+    angular.element(document).ready(function () {
         var doc = angular.element(document);
         var injector = doc.injector();
-        try {
 
-            // If the app is already bootstrapped then an error will be thrown
-            angular.bootstrap(document, ['core', 'metadataTool', 'ui.bootstrap']);
-            
-            if(!window.stompClient.connected) {
-                AlertService = injector.get('AlertService');
-                AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
+        try {
+            //If the app is already bootstrapped then an error will be thrown
+            angular.bootstrap(document, ['metadataTool', 'core', 'ui.bootstrap']);
+
+            if (!window.stompClient.connected) {
+                unreachableAlert(injector);
             }
         } catch (e) {
-
             /*
              * If websockets dissconnect the app will attempt to re-bootstrap. Since the app is already running we will
              * end up in this block, and can generate an error indicating the disconnect.
              */
-            if(typeof injector != 'undefined') {
-                AlertService = injector.get('AlertService');
-                AlertService.add({type: "ERROR", message: "Web service cannot be reached."}, "/app/errors");
-            }
+            console.log("Angular could not bootstrap", e);
+            unreachableAlert(injector);
         }
     });
 
