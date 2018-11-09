109066f29d3c502e47000d542efd82ea5d01c5f9
Kevin Day <kday@library.tamu.edu>
2018-11-09 17:18:44 -0600

    Implement repo test mocks (the skeleton structure)

--- tests/mocks/repos/mockDocumentRepo.js
+++ tests/mocks/repos/mockDocumentRepo.js
@@ -1,104 +1,312 @@
 var mockDocumentRepo1 = {
-	'HashMap':{
-		'0':{
-			file: null,
-			filename: "disseration001.txt",
-			status: "Open",
-			annotator: "111111111"
-		},
-		'1':{
-			file: null,
-			filename: "disseration002.txt",
-			status: "Open",
-			annotator: "222222222"
-		},
-		'2':{
-			file: null,
-			filename: "disseration003.txt",
-			status: "Open",
-			annotator: "333333333"
-		}
-	}
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration001.txt",
+            name: "Dissertation 001",
+            status: "Open",
+            annotator: "111111111"
+        },
+        '1':{
+            file: null,
+            filename: "disseration002.txt",
+            name: "Dissertation 002",
+            status: "Open",
+            annotator: "222222222"
+        },
+        '2':{
+            file: null,
+            filename: "disseration003.txt",
+            name: "Dissertation 003",
+            status: "Open",
+            annotator: "333333333"
+        }
+    }
 };
 
 var mockDocumentRepo2 = {
-	'HashMap':{
-		'0':{
-			file: null,
-			filename: "disseration002.txt",
-			status: "Open",
-			annotator: "222222222"
-		},
-		'1':{
-			file: null,
-			filename: "disseration003.txt",
-			status: "Open",
-			annotator: "333333333"
-		},
-		'2':{
-			file: null,
-			filename: "disseration004.txt",
-			status: "Open",
-			annotator: "444444444"
-		}
-	}
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration002.txt",
+            status: "Open",
+            annotator: "222222222"
+        },
+        '1':{
+            file: null,
+            filename: "disseration003.txt",
+            status: "Open",
+            annotator: "333333333"
+        },
+        '2':{
+            file: null,
+            filename: "disseration004.txt",
+            status: "Open",
+            annotator: "444444444"
+        }
+    }
 };
 
 var mockDocumentRepo3 = {
-	'HashMap':{
-		'0':{
-			file: null,
-			filename: "disseration003.txt",
-			status: "Open",
-			annotator: "333333333"
-		}
-	}
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration003.txt",
+            status: "Open",
+            annotator: "333333333"
+        }
+    }
 };
 
-angular.module('mock.documentRepo', []).
-    service('DocumentRepo', function($q) {
-    	
-    	var self;
-    	
-    	var DocumentRepo = function(futureData) {
-    		self = this;
-			
-    		if(!futureData.$$state) {
-    			angular.extend(self, futureData);
-    			return;
-    		}
+angular.module('mock.documentRepo', []).service('DocumentRepo', function($q) {
+    var repo = this;
+    var defer;
+    var validations = {};
+    var validationResults = {};
+    var originalList;
 
-    		futureData.then(null, null, function(data) {
-    			angular.extend(self, data);	
-    		});
+    var payloadResponse = function (payload) {
+        return defer.resolve({
+            body: angular.toJson({
+                meta: {
+                    status: 'SUCCESS'
+                },
+                payload: payload
+            })
+        });
+    };
 
-    	}
-    	
-    	DocumentRepo.get = function() {
-            return new DocumentRepo(mockDocumentRepo1);
-        };
-        
-        DocumentRepo.set = function(data) {
-        	angular.extend(self, data);
-        };
-        
-        DocumentRepo.fetch = function() {
-        	return $q(function(resolve) {            	
-            	resolve(mockDocumentRepo3);
-            });
-        }; 
-        
-        DocumentRepo.listen = function() {
-        	return $q(function(resolve) {            	
-            	resolve(mockDocumentRepo3);
-            });
-        }; 
-        
-        DocumentRepo.ready = function() {
-        	return $q(function(resolve) {            	
-            	resolve(mockDocumentRepo3);
-            });
-    	};
-        
-        return DocumentRepo;
+    var messageResponse = function (message) {
+        return defer.resolve({
+            body: angular.toJson({
+                meta: {
+                    status: 'SUCCESS',
+                    message: message
+                }
+            })
+        });
+    };
+
+    repo.mockedList = [];
+
+    repo.mock = function(toMock) {
+        repo.mockedList = toMock;
+        this.originalList = toMock;
+    };
+
+    repo.mock(mockDocumentRepo1);
+
+    repo.add = function (modelJson) {
+        if (!repo.contains(modelJson)) {
+            repo.mockedList.push(modelJson);
+        }
+    };
+
+    repo.addAll = function (modelJsons) {
+        for (var i in modelJsons) {
+            repo.add(modelJsons[i]);
+        }
+    };
+
+    repo.clearValidationResults = function () {
+        validationResults = {};
+    };
+
+    repo.create = function (model) {
+        defer = $q.defer();
+        model.id = repo.mockedList.length + 1;
+        repo.mockedList.push(angular.copy(model));
+        payloadResponse(model);
+        return defer.promise;
+    };
+
+    repo.contains = function (model) {
+        var found = false;
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id === model.id) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    };
+
+    repo.count = function () {
+        return repo.mockedList.length;
+    };
+
+    repo.delete = function (model) {
+        defer = $q.defer();
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id === model.id) {
+                repo.mockedList.splice(i, 1);
+                break;
+            }
+        }
+        payloadResponse();
+        return defer.promise;
+    };
+
+    repo.deleteById = function (id) {
+        defer = $q.defer();
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id === id) {
+                repo.mockedList.splice(i, 1);
+                break;
+            }
+        }
+        payloadResponse();
+        return defer.promise;
+    };
+
+    repo.fetch = function () {
+        defer = $q.defer();
+        payloadResponse(mockDocumentRepo3);
+        return defer.promise;
+    };
+
+    repo.findById = function (id) {
+        var found;
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id == id) {
+                found = angular.copy(repo.mockedList[i]);
+            }
+        }
+        return found;
+    };
+
+    repo.get = function (projectName, documentName) {
+        defer = $q.defer();
+        var found;
+        for (var document in repo.mockedList) {
+            if (document.project === projectName && document.name === documentName) {
+              found = document;
+              break;
+            }
+        }
+        payloadResponse(found);
+        return defer.promise;
+    };
+
+    repo.getAll = function () {
+        return angular.copy(repo.mockedList);
+    };
+
+    repo.getAllByRole = function (role) {
+        var found;
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].role == role) {
+                found = angular.copy(repo.mockedList[i]);
+                break;
+            }
+        }
+
+        return found;
+    };
+
+    repo.getAllFiltered = function(predicate) {
+        var data = repo.mockedList;
+        var filteredData = [];
+        // TODO
+        return filteredData;
+    };
+
+    repo.getAssignableUsers = function (roles) {
+        var payload = {};
+        defer = $q.defer();
+        // TODO
+        payloadResponse(payload);
+        return defer.promise;
+    };
+
+    repo.getContents = function () {
+        return angular.copy(repo.mockedList);
+    };
+
+    repo.getEntityName = function () {
+        return "DocumentRepo";
+    };
+
+    repo.getValidations = function () {
+        return angular.copy(validations);
+    };
+
+    repo.getValidationResults = function () {
+        return angular.copy(validationResults);
+    };
+
+    repo.listen = function (cbOrActionOrActionArray, cb) {
+        defer = $q.defer();
+        payloadResponse(mockDocumentRepo3);
+        return defer.promise;
+    };
+
+    repo.page = function (number, size, field, direction, filters) {
+        defer = $q.defer();
+        // TODO
+        payloadResponse(angular.copy(repo.mockedList));
+        return defer.promise;
+    };
+
+    repo.ready = function () {
+        defer = $q.defer();
+        payloadResponse(mockDocumentRepo3);
+        return defer.promise;
+    };
+
+    repo.remove = function (modelToRemove) {
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id === modelToRemove.id) {
+                repo.mockedList.splice(i, 1);
+                break;
+            }
+        }
+    };
+
+    repo.reset = function () {
+        defer = $q.defer();
+        repo.mockedList = repo.originalList;
+        payloadResponse();
+        return defer.promise;
+    };
+
+    repo.save = function (model) {
+        defer = $q.defer();
+        // TODO
+        payloadResponse({});
+        return defer.promise;
+    };
+
+    repo.saveAll = function () {
+        angular.forEach(repo.mockedList, function (model) {
+            repo.save(model);
+        });
+    };
+
+    repo.setToDelete = function (id) {
+        // TODO
+    };
+    repo.setToUpdate = function (id) {
+        // TODO
+    };
+
+    repo.unshift = function (modelJson) {
+        // TODO
+    };
+
+    repo.update = function (model) {
+        defer = $q.defer();
+        var updated;
+        for (var i in repo.mockedList) {
+            if (repo.mockedList[i].id === model.id) {
+                updated = angular.copy(repo.mockedList[i]);
+                angular.extend(updated, model);
+                break;
+            }
+        }
+        payloadResponse(updated);
+        return defer.promise;
+    };
+
+    return repo;
 });
--- tests/mocks/repos/mockResourceRepo.js
+++ tests/mocks/repos/mockResourceRepo.js
@@ -1,73 +1,61 @@
-var mockUserRepo1 = {
-    'HashMap': {
-        '0': {
-            "uin": "123456789",
-            "lastName": "Daniels",
-            "firstName": "Jack",
-            "role": "ROLE_ADMIN"
+var mockResourceRepo1 = {
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration001.txt",
+            status: "Open",
+            annotator: "111111111"
         },
-        '1': {
-            "uin": "987654321",
-            "lastName": "Daniels",
-            "firstName": "Jill",
-            "role": "ROLE_USER"
+        '1':{
+            file: null,
+            filename: "disseration002.txt",
+            status: "Open",
+            annotator: "222222222"
         },
-        '2': {
-            "uin": "192837465",
-            "lastName": "Smith",
-            "firstName": "Jacob",
-            "role": "ROLE_USER"
+        '2':{
+            file: null,
+            filename: "disseration003.txt",
+            status: "Open",
+            annotator: "333333333"
         }
     }
 };
 
-var mockUserRepo2 = {
-    'HashMap': {
-        '0': {
-            "uin": "321654987",
-            "lastName": "Daniels",
-            "firstName": "John",
-            "role": "ROLE_ADMIN"
+var mockResourceRepo2 = {
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration002.txt",
+            status: "Open",
+            annotator: "222222222"
         },
-        '1': {
-            "uin": "789456123",
-            "lastName": "Daniels",
-            "firstName": "Joann",
-            "role": "ROLE_USER"
+        '1':{
+            file: null,
+            filename: "disseration003.txt",
+            status: "Open",
+            annotator: "333333333"
         },
-        '2': {
-            "uin": "564738291",
-            "lastName": "Smith",
-            "firstName": "Joseph",
-            "role": "ROLE_USER"
+        '2':{
+            file: null,
+            filename: "disseration004.txt",
+            status: "Open",
+            annotator: "444444444"
         }
     }
 };
 
-var mockUserRepo3 = {
-    'HashMap': {
-        '0': {
-            "uin": "111111111",
-            "lastName": "User1",
-            "firstName": "Test",
-            "role": "ROLE_ADMIN"
-        },
-        '1': {
-            "uin": "222222222",
-            "lastName": "User2",
-            "firstName": "Test",
-            "role": "ROLE_USER"
-        },
-        '2': {
-            "uin": "333333333",
-            "lastName": "User3",
-            "firstName": "Test",
-            "role": "ROLE_USER"
+var mockResourceRepo3 = {
+    'HashMap':{
+        '0':{
+            file: null,
+            filename: "disseration003.txt",
+            status: "Open",
+            annotator: "333333333"
         }
     }
 };
 
-angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
+angular.module('mock.resourceRepo', []).service('ResourceRepo', function($q) {
     var repo = this;
     var defer;
     var validations = {};
@@ -103,7 +91,7 @@ angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
         this.originalList = toMock;
     };
 
-    repo.mock(mockUserRepo1);
+    repo.mock(mockResourceRepo1);
 
     repo.add = function (modelJson) {
         if (!repo.contains(modelJson)) {
@@ -168,6 +156,12 @@ angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
         return defer.promise;
     };
 
+    repo.fetch = function () {
+        defer = $q.defer();
+        payloadResponse(mockResourceRepo3);
+        return defer.promise;
+    };
+
     repo.findById = function (id) {
         var found;
         for (var i in repo.mockedList) {
@@ -180,6 +174,19 @@ angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
 
     repo.getAll = function () {
         return angular.copy(repo.mockedList);
+    };
+
+    repo.getAllByProjectNameAndDocumentName = function(projectName, documentName) {
+        defer = $q.defer();
+        var found = [];
+        for (var i in repo.mockedList) {
+            // TODO
+            if (repo.mockedList[i].projectName === projectName && repo.mockedList[i].documentName === documentName) {
+                found.push(angular.copy(repo.mockedList[i]));
+            }
+        }
+        payloadResponse(found);
+        return defer.promise;
     };
 
     repo.getAllByRole = function (role) {
@@ -214,7 +221,7 @@ angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
     };
 
     repo.getEntityName = function () {
-        return "UserRepo";
+        return "ResourceRepo";
     };
 
     repo.getValidations = function () {
@@ -226,12 +233,14 @@ angular.module('mock.userRepo', []).service('UserRepo', function ($q) {
     };
 
     repo.listen = function (cbOrActionOrActionArray, cb) {
-        // TODO
+        defer = $q.defer();
+        payloadResponse(mockResourceRepo3);
+        return defer.promise;
     };
 
     repo.ready = function () {
         defer = $q.defer();
-        payloadResponse();
+        payloadResponse(mockResourceRepo3);
         return defer.promise;
     };
 
