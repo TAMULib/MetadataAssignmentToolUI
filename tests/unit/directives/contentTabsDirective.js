10db5835d63b54c84e28a074d8bdd5a7d9702a82
Kevin Day <kday@library.tamu.edu>
2018-11-14 10:59:04 -0600

    Implement directive tests (the skeleton structure)

---
+++ tests/unit/directives/contentTabsDirective.js
@@ -0,0 +1,27 @@
+describe('directive: contentTabs', function () {
+    var compile, controller, directive, httpBackend, scope, templateCache;
+
+    beforeEach(function() {
+        module('core');
+        module('metadataTool');
+
+        inject(function ($compile, $httpBackend, $rootScope, $templateCache) {
+            compile = $compile;
+            httpBackend = $httpBackend;
+            scope = $rootScope.$new();
+            templateCache = $templateCache;
+
+            httpBackend.whenGET('views/directives/contentTabs.html').respond('<div></div>');
+
+            directive = angular.element('<contenttabs ng-model="model"></contenttabs>');
+            compile(directive)(scope);
+            scope.$digest();
+        });
+    });
+
+    describe('Is the directive defined', function () {
+        it('should be defined', function () {
+            expect(directive).toBeDefined();
+        });
+    });
+});
---
+++ tests/unit/directives/contentViewerDirective.js
@@ -0,0 +1,29 @@
+describe('directive: inputDirective', function () {
+    var compile, controller, directive, httpBackend, scope, templateCache;
+
+    beforeEach(function() {
+        module('core');
+        module('metadataTool');
+
+        inject(function ($compile, $httpBackend, $rootScope, $templateCache) {
+            compile = $compile;
+            httpBackend = $httpBackend;
+            scope = $rootScope.$new();
+            templateCache = $templateCache;
+
+            for (var i in ['default', 'image', 'pdf', 'seadragon', 'text'] ) {
+                httpBackend.whenGET('views/directives/viewers/' + i + 'Viewer.html').respond('<div></div>');
+            }
+
+            directive = angular.element('<contentviewer ng-model="model"></contentviewer>');
+            compile(directive)(scope);
+            scope.$digest();
+        });
+    });
+
+    describe('Is the directive defined', function () {
+        it('should be defined', function () {
+            expect(directive).toBeDefined();
+        });
+    });
+});
