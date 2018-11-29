6681f55233f7f3e857afe0fff5404607b4075495
Kevin Day <kday@library.tamu.edu>
2018-11-29 12:56:33 -0600

    Relocate allowableRoles (now assignableRoles) and canDelete outside of isAdmin() and isManager() conditional

--- app/controllers/userRepoController.js
+++ app/controllers/userRepoController.js
@@ -6,6 +6,43 @@ metadataTool.controller('UserRepoController', function ($controller, $location, 
 
     UserService.userReady().then(function() {
 
+    $scope.assignableRoles = function(userRole) {
+        if($scope.isAdmin()) {
+            return ['ROLE_ADMIN','ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
+        }
+        else if($scope.isManager()) {
+            if(userRole == 'ROLE_ADMIN') {
+                return ['ROLE_ADMIN'];
+            }
+            return ['ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
+        }
+        else {
+            return [userRole];
+        }
+    };
+
+    $scope.canDelete = function(user) {
+        var canDelete;
+        if($scope.isAdmin()) {
+            canDelete = true;
+        }
+        else if($scope.isManager()) {
+            if(user.role == "ROLE_ADMIN") {
+                canDelete = false;
+            }
+            else {
+                canDelete = true;
+            }
+        }
+        else {
+            canDelete = false;
+        }
+        if(user.uin == $scope.user.uin) {
+            canDelete = false;
+        }
+        return canDelete;
+    };
+
     if($scope.isAdmin() || $scope.isManager()) {
 
         var UserRepo = $injector.get("UserRepo");
@@ -31,39 +68,8 @@ metadataTool.controller('UserRepoController', function ($controller, $location, 
             }
         };
 
-        $scope.allowableRoles = function(userRole) {
-            if($scope.isAdmin()) {
-                return ['ROLE_ADMIN','ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
-            }
-            else if($scope.isManager()) {
-                if(userRole == 'ROLE_ADMIN') {
-                    return ['ROLE_ADMIN'];
-                }
-                return ['ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
-            }
-        };
-
         $scope.delete = function(user) {
             user.delete();
-        };
-
-        $scope.canDelete = function(user) {
-            var canDelete;
-            if($scope.isAdmin()) {
-                canDelete = true;
-            }
-            else if($scope.isManager()) {
-                if(user.role == "ROLE_ADMIN") {
-                    canDelete = false;
-                }
-                else {
-                    canDelete = true;
-                }
-            }
-            if(user.uin == $scope.user.uin) {
-                canDelete = false;
-            }
-            return canDelete;
         };
 
         UserRepo.listen(function(response) {
