5efdd97c8db757970ee9d9a4c1903d88e9a9c449
Kevin Day <kday@library.tamu.edu>
2018-11-29 13:05:23 -0600

    Fix indentation of UserRepoController

--- app/controllers/userRepoController.js
+++ app/controllers/userRepoController.js
@@ -6,78 +6,78 @@ metadataTool.controller('UserRepoController', function ($controller, $location, 
 
     UserService.userReady().then(function() {
 
-    $scope.assignableRoles = function(userRole) {
-        if($scope.isAdmin()) {
-            return ['ROLE_ADMIN','ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
-        }
-        else if($scope.isManager()) {
-            if(userRole == 'ROLE_ADMIN') {
-                return ['ROLE_ADMIN'];
+        $scope.assignableRoles = function(userRole) {
+            if($scope.isAdmin()) {
+                return ['ROLE_ADMIN','ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
             }
-            return ['ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
-        }
-        else {
-            return [userRole];
-        }
-    };
-
-    $scope.canDelete = function(user) {
-        var canDelete;
-        if($scope.isAdmin()) {
-            canDelete = true;
-        }
-        else if($scope.isManager()) {
-            if(user.role == "ROLE_ADMIN") {
-                canDelete = false;
+            else if($scope.isManager()) {
+                if(userRole == 'ROLE_ADMIN') {
+                    return ['ROLE_ADMIN'];
+                }
+                return ['ROLE_MANAGER','ROLE_ANNOTATOR','ROLE_USER'];
             }
             else {
+                return [userRole];
+            }
+        };
+
+        $scope.canDelete = function(user) {
+            var canDelete;
+            if($scope.isAdmin()) {
                 canDelete = true;
             }
-        }
-        else {
-            canDelete = false;
-        }
-        if(user.uin == $scope.user.uin) {
-            canDelete = false;
-        }
-        return canDelete;
-    };
-
-    if($scope.isAdmin() || $scope.isManager()) {
-
-        var UserRepo = $injector.get("UserRepo");
-
-        $scope.userUpdated = {};
-
-        $scope.users = UserRepo.getAll();
-
-        $scope.updateRole = function(user) {
-
-            angular.extend($scope.userUpdated, user);
-
-            user.save();
-
-            if($scope.user.username == user.username) {
-                if(user.role == 'ROLE_ANNOTATOR') {
-                    $location.path('/assignments');
+            else if($scope.isManager()) {
+                if(user.role == "ROLE_ADMIN") {
+                    canDelete = false;
                 }
-                else if(user.role == 'ROLE_USER') {
-                    $location.path('/myview');
+                else {
+                    canDelete = true;
                 }
-                else {}
             }
+            else {
+                canDelete = false;
+            }
+            if(user.uin == $scope.user.uin) {
+                canDelete = false;
+            }
+            return canDelete;
         };
 
-        $scope.delete = function(user) {
-            user.delete();
-        };
+        if($scope.isAdmin() || $scope.isManager()) {
 
-        UserRepo.listen(function(response) {
+            var UserRepo = $injector.get("UserRepo");
+
             $scope.userUpdated = {};
-            $route.reload();
-        });
 
-    }
+            $scope.users = UserRepo.getAll();
+
+            $scope.updateRole = function(user) {
+
+                angular.extend($scope.userUpdated, user);
+
+                user.save();
+
+                if($scope.user.username == user.username) {
+                    if(user.role == 'ROLE_ANNOTATOR') {
+                        $location.path('/assignments');
+                    }
+                    else if(user.role == 'ROLE_USER') {
+                        $location.path('/myview');
+                    }
+                    else {}
+                }
+            };
+
+            $scope.delete = function(user) {
+                user.delete();
+            };
+
+            UserRepo.listen(function(response) {
+                $scope.userUpdated = {};
+                $route.reload();
+            });
+
+        }
 
     });
 
