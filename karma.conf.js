48fc3f8527715cb976523e64ba948115a721fd00
Kevin Day <kday@library.tamu.edu>
2019-12-17 14:22:20 -0600

    Update karma config
    Remove duplicate includes.
    Make ordering structure more similar to other projects so that they can be more easily compared.

--- karma.conf.js
+++ karma.conf.js
@@ -15,23 +15,32 @@ module.exports = function (config) {
       "app/config/apiMapping.js",
 
       "app/node_modules/jquery/dist/jquery.js",
-      "app/node_modules/angular/angular.js",
-      "app/node_modules/angular-mocks/angular-mocks.js",
-      "app/node_modules/angular-route/angular-route.js",
-      "app/node_modules/angular-sanitize/angular-sanitize.min.js",
+      "app/node_modules/bootstrap/dist/js/bootstrap.js",
 
-      "app/node_modules/ng-table/bundles/ng-table.min.js",
-      "app/node_modules/ng-csv/build/ng-csv.min.js",
+      "app/node_modules/sockjs-client/dist/sockjs.js",
+      "app/node_modules/stompjs/lib/stomp.js",
+
+      "app/node_modules/angular/angular.js",
+
+      "app/node_modules/angular-sanitize/angular-sanitize.js",
+      "app/node_modules/angular-route/angular-route.js",
+      "app/node_modules/angular-loader/angular-loader.js",
+      "app/node_modules/angular-messages/angular-messages.js",
+      "app/node_modules/angular-mocks/angular-mocks.js",
+
+      "app/node_modules/ng-csv/build/ng-csv.js",
+
+      "app/node_modules/ng-table/bundles/ng-table.js",
+
       "app/node_modules/ng-file-upload/dist/ng-file-upload-shim.js",
       "app/node_modules/ng-file-upload/dist/ng-file-upload.js",
 
-      "app/node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js",
-
       "app/node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
 
-      "app/node_modules/openseadragon/build/openseadragon/openseadragon.min.js",
+      "app/node_modules/openseadragon/build/openseadragon/openseadragon.js",
+      "app/node_modules/ng-openseadragon/build/angular-openseadragon.js",
 
-      "app/node_modules/ng-openseadragon/dist/ng-openseadragon.min.js",
+      "app/node_modules/jasmine-promise-matchers/dist/jasmine-promise-matchers.js",
 
       "app/node_modules/weaver-ui-core/app/config/coreConfig.js",
 
@@ -40,9 +49,6 @@ module.exports = function (config) {
       "app/node_modules/weaver-ui-core/app/core.js",
 
       "app/node_modules/weaver-ui-core/app/**/*.js",
-
-      "app/node_modules/sockjs-client/dist/sockjs.min.js",
-      "app/node_modules/stompjs/lib/stomp.min.js",
 
       "tests/testSetup.js",
 
